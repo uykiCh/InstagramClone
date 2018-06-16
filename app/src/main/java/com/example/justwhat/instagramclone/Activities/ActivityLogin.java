c6e017016e0b9758fcab3f3a5f40088762df514f
usdhlJnljnJjnLJNuH78h99h <brat0172@gmail.com>
2018-06-16 12:53:15 +0300

    Demo

--- app/src/main/java/com/example/justwhat/instagramclone/Activities/ActivityLogin.java
+++ app/src/main/java/com/example/justwhat/instagramclone/Activities/ActivityLogin.java
@@ -1,16 +1,16 @@
-package com.example.justwhat.instagramclone;
+package com.example.justwhat.instagramclone.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.justwhat.instagramclone.R;
+
+public class ActivityLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    //
 
 }
